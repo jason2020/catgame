05fa2f3ce6cac0f73b40588a2b4512d325197cc1
jason2020 <jasontay@jasons-mbp.lan>
2020-11-22 16:33:01 -1000

    added picture taking

--- script.js
+++ script.js
@@ -51,5 +51,16 @@ function spawnCat() {
         </div>`);
 }
 
-
+$("#takePic").on("click", function() {
+    html2canvas($('#game'), 
+    {
+      onrendered: function (canvas) {
+        var a = document.createElement('a');
+        // toDataURL defaults to png, so we need to request a jpeg, then convert for file download.
+        a.href = canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
+        a.download = 'somefilename.jpg';
+        a.click();
+      }
+    });
+})
     
